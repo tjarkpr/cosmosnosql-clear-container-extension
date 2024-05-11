11ae0baa8a157795b4bfe85a2a1ee3d28cdad840
Tjark Prokoph <56918325+tjarkpr@users.noreply.github.com>
2024-05-11 16:36:25 +0200

    Changed auth provider and did some refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,79 +1,79 @@
-import * as vscode from 'vscode';
-import { TreeView } from 'vscode';
-import { InteractiveBrowserCredential } from "@azure/identity";
+import { ExtensionContext, commands, window, TreeView } from 'vscode';
+import { UpdateVSCodeContextValue, LogError, GetFullExtensionName } from './helper/vscode-helper';
+import { TokenCredential } from "@azure/identity";
+import { VSCodeAzureSubscriptionProvider } from "@microsoft/vscode-azext-azureauth";
 import { AzureResourceProvider, AzureResource } from './provider/azure-resource-tree-provider';
-import { ClearContainerAdapter } from './adapter/clear-container-adapter';
+import { CosmosClientAdapter } from './adapter/cosmos-client-sdk-adapter';
 
-export function activate(context: vscode.ExtensionContext) {
-	initializeWorkspaceState(context);
-	initializeContext();
 
-	var treeResourceProvider = new AzureResourceProvider();
-	var clearContainerAdapter = new ClearContainerAdapter(treeResourceProvider);
-	var treeView : TreeView<AzureResource> | null = null;
+const IS_LOGGED_INTO_AZURE_CONTEXT_KEY: string = "isLoggedIntoAzure";
+const LOGIN_TO_AZURE_COMMAND_NAME: string = "loginToAzure";
+const LOGOUT_FROM_AZURE_COMMAND_NAME: string = "logoutFromAzure";
+const RELAOD_COMMAND_NAME: string = "reload";
+const CLEAR_CONTAINER_COMMAND_NAME: string = "clearContainer";
+const AZURE_TREE_VIEW_NAME: string = "azure-resource-tree-view";
 
-	context.subscriptions.push(vscode.commands.registerCommand('cosmosnosql-clear-container-extension.loginToAzure', () => {
+export function activate(context: ExtensionContext): void {
+	UpdateVSCodeContextValue(IS_LOGGED_INTO_AZURE_CONTEXT_KEY, false);
+
+	const _subscriptionProvider: VSCodeAzureSubscriptionProvider = new VSCodeAzureSubscriptionProvider();
+	const _azureResourceProvider: AzureResourceProvider = new AzureResourceProvider(_subscriptionProvider);
+	const _cosmosClientAdapter: CosmosClientAdapter = new CosmosClientAdapter(_azureResourceProvider);
+	
+	let _azureResourceTreeView: TreeView<AzureResource> | null = null;
+	let _credentials: Map<string, TokenCredential> = new Map<string, TokenCredential>();
+
+	context.subscriptions.push(
+		commands.registerCommand(
+			GetFullExtensionName(LOGIN_TO_AZURE_COMMAND_NAME), async () => 
+	{
 		try {
-			var credentials = new InteractiveBrowserCredential({});
-			treeResourceProvider.updateCredentials(credentials);
-			clearContainerAdapter.updateCredentials(credentials);
-			context.workspaceState.update('isLoggedIntoAzure', true);
-			updateContext('isLoggedIntoAzure', true);
-			treeView = vscode.window.createTreeView('azure-resource-tree-view', {
-				treeDataProvider: treeResourceProvider
+			if (!await _subscriptionProvider.signIn()) { throw Error("Login not successfull"); }
+
+			const subscriptions = await _subscriptionProvider.getSubscriptions();
+			subscriptions.forEach(s => {
+				_credentials.set(s.subscriptionId, s.credential);
+			})
+
+			_azureResourceProvider.updateCredentials(_credentials);
+			UpdateVSCodeContextValue(IS_LOGGED_INTO_AZURE_CONTEXT_KEY, true);
+
+			_azureResourceTreeView = window.createTreeView(AZURE_TREE_VIEW_NAME, {
+				treeDataProvider: _azureResourceProvider
 			});
-		} catch (error) {
-			logError(error);
-		}
+		} catch (error) { LogError(error); }
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('cosmosnosql-clear-container-extension.logoutFromAzure', () => {
-		treeResourceProvider.updateCredentials(null);
-		clearContainerAdapter.updateCredentials(null);
-		context.workspaceState.update('isLoggedIntoAzure', false);
-		updateContext('isLoggedIntoAzure', false);
-		treeResourceProvider.refreshTree();
+	context.subscriptions.push(
+		commands.registerCommand(
+			GetFullExtensionName(LOGOUT_FROM_AZURE_COMMAND_NAME), () => 
+	{
+		_credentials.clear()
+		_azureResourceProvider.updateCredentials(null);
+		UpdateVSCodeContextValue(IS_LOGGED_INTO_AZURE_CONTEXT_KEY, false);
+		_azureResourceProvider.refreshTree();
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('cosmosnosql-clear-container-extension.reload', () => {
-		var selection = treeView?.selection ?? [];
+	context.subscriptions.push(
+		commands.registerCommand(
+			GetFullExtensionName(RELAOD_COMMAND_NAME), () => 
+	{
+		const selection = _azureResourceTreeView?.selection ?? [];
 		selection.forEach(element => {
-			treeResourceProvider.refresh(element);
+			_azureResourceProvider.refresh(element);
 		});
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('cosmosnosql-clear-container-extension.clearContainer', async () => {
-		var selection = treeView?.selection ?? [];
+	context.subscriptions.push(
+		commands.registerCommand(
+			GetFullExtensionName(CLEAR_CONTAINER_COMMAND_NAME), async () => 
+	{
+		const selection = _azureResourceTreeView?.selection ?? [];
 		for (const element of selection) {
-			await clearContainerAdapter.clear(element);
-			treeResourceProvider.refresh(element);
+			await _cosmosClientAdapter.clearSelection(element);
+			_azureResourceProvider.refresh(element);
 		}
 	}));
-}
-
-function initializeWorkspaceState(context: vscode.ExtensionContext) {
-	context.workspaceState.update('isLoggedIntoAzure', false);
-}
-
-function initializeContext() {
-	updateContext('isLoggedIntoAzure', false);
-}
-
-function updateContext(key: string, value: any) {
-	vscode.commands.executeCommand(
-		'setContext', 
-		'cosmosnosql-clear-container-extension.' + key, 
-		value);
-}
-
-function logError(error: any) {
-	if (error instanceof Error) {
-		vscode.window.showErrorMessage(error.message);
-	}
-	if (typeof error === 'string') {
-		vscode.window.showErrorMessage(error);
-	}
-	console.error(error);
 }
 
 export function deactivate() {}
